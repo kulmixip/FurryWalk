ac7987100cab2b7da8e4c10154d21e02a5041048
kulmixip <anders_b96@hotmail.com>
2024-10-23 12:21:47 -0300

    First commit

---
+++ app/src/main/java/com/example/composeproject/ui/theme/Homescreen.kt
@@ -0,0 +1,26 @@
+package com.example.myapp.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { navController.navigate("details") }) {
+            Text(text = "Back to Details Screen")
+        }
+        Text(text = "This is the Home screen")
+    }
+}
