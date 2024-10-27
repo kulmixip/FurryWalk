d3e0604d0012b58cc27bfb8d67ec8316a18ab024
HanneEriksen <bergumhanne@hotmail.com>
2024-10-27 14:16:41 +0100

    test

--- app/src/main/java/com/example/composeproject/screens/Homescreen.kt
+++ app/src/main/java/com/example/composeproject/screens/Homescreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 
 @Composable
@@ -21,6 +22,15 @@ fun HomeScreen(navController: NavHostController) {
         Button(onClick = { navController.navigate("details") }) {
             Text(text = "Back to Details Screen")
         }
-        Text(text = "This is the Home screen")
+        Text(text = "This is the Home screen1234")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    ComposeProjectTheme {
+        HomeScreen("Hanne")
     }
 }
